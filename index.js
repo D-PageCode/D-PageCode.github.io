985e1e8533a3ae2ab4bc2e2301fe5f863ff63e39
T-PageCode <cdt20260@outlook.com>
2026-08-09 18:58:02 +0800

    v1.0.0，最初版本

---
+++ index.js
@@ -0,0 +1,42 @@
+document.addEventListener("contextmenu",(e) => {
+    e.preventDefault();
+})
+const div1 = document.getElementById('div1');
+const text1 = document.getElementById('text1');
+const text2 = document.getElementById('text2');
+
+let timer = null;
+div1.addEventListener('mouseenter', () => {
+    if (timer) {
+        clearTimeout(timer);
+        timer = null;
+    }
+    text1.style.opacity = '0';
+    timer = setTimeout(() => {
+        text2.style.opacity = '1';
+        timer = null;
+    }, 400);
+});
+
+div1.addEventListener('mouseleave', () => {
+    if (timer) {
+        clearTimeout(timer);
+        timer = null;
+    }
+    text2.style.opacity = '0';
+    timer = setTimeout(() => {
+        text1.style.opacity = '1';
+        timer = null;
+    }, 400);
+});
+let fakeEventCounter = 0;
+const idPrinter = setInterval(() => {
+    fakeEventCounter++;
+    if (fakeEventCounter % 2 === 1) {
+        div1.dispatchEvent(new Event('mouseenter'));
+        console.log(timer);
+    } else {
+        div1.dispatchEvent(new Event('mouseleave'));
+        console.log(timer);
+    }
+},1000000);
